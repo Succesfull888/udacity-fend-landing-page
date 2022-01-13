8328b74593d6311e0031a1f1fd6bce70bc339551
Succesfull888 <68223774+Succesfull888@users.noreply.github.com>
2022-01-13 23:10:31 +0300

    Add files via upload

---
+++ landing-page/README.md
@@ -0,0 +1,22 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+
+* This is the seconnd project of udacity front-end web developer nanodegree program. In this course students learn how to use html css and javascript in expert level.In this project navbar and some functions are created by using javascript.
+
+# What students are required in this project
+* Creating navbar (<li>sectionNumber</li> should automatically added,not using html);
+* Using scroll(when user click link page should scrolled to the appropriate section);
+* Adding active class to the sections and navbar elements.When user scrolls the page it should be automatically change active class(getBoundingClientRect() method is recommended for this task)
+
+# Getting started
+## Installation
+
+Clone repository using git or download manually.
+Open index.html in your browser 
+
---
+++ landing-page/js/app.js
@@ -0,0 +1,97 @@
+/**
+ * Define Global Variables
+ * 
+*/
+const sections=document.querySelectorAll('section');
+const navbar=document.querySelector('#navbar__list');
+let NavTag = document.getElementById("navbar_header");
+let sectionslength = sections.length;
+let sectionspositions = [];
+let prePosition = 0;
+let position = 0;
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions  
+ * 
+*/
+
+// build the nav
+function newElement(){
+    for(let i=1;i<=sections.length;i++){
+        const li=document.createElement('li');
+        navbar.appendChild(li);
+        const anchor=document.createElement('a');
+        li.appendChild(anchor);
+        anchor.setAttribute('class','menu__link');
+        anchor.setAttribute('href','#section'+i);
+        anchor.textContent='section '+i;
+
+        
+    };
+};  
+newElement();
+
+
+
+// Add class 'active' to section when near top of viewport
+var actives = navbar.getElementsByClassName("menu__link");
+
+for (var i = 0; i < actives.length; i++) {
+    actives[i].addEventListener("click", function() {
+      var hozirgi = document.getElementsByClassName("active");
+  
+      if (hozirgi.length > 0) {
+        hozirgi[0].className = hozirgi[0].className.replace(" active", "");
+        
+      }
+  
+      this.className += " active";
+    });
+  }
+// Set sections as active
+
+
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+ 
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+document.addEventListener("scroll", () => {
+	currentPosition = this.scrollY;
+	// Section Positions
+	sectionspositions = [];
+	sections.forEach((element) => sectionspositions.push(element.getBoundingClientRect().top + 50));
+
+	// Adding and removing active sections
+	let addIndex = sectionspositions.findIndex((element) => element > 0);
+	for (let i = 0; i < sectionslength; i++) {
+		if (addIndex === i) {
+			document.querySelector(`#section${addIndex + 1}`).classList.add("your-active-class");
+		} else {
+			document.querySelector(`#section${i + 1}`).removeAttribute("class");
+		}
+	}
+});
+
+// Scroll to section on link click
+
+
+
